ec83e547d2d87828755e6f99f34d11364ac3b201
JiayueZ <studentzhang2002@163.com>
2023-12-27 17:12:35 +0800

    feat: add collect word spec page

---
+++ lib/collect/collect_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:words/collect/widget/collect_spec.dart';
+import 'package:words/word/model/word.dart';
+
+class CollectPage extends StatefulWidget {
+  const CollectPage({super.key});
+
+  @override
+  State<CollectPage> createState() => _CollectPageState();
+}
+
+class _CollectPageState extends State<CollectPage> {
+  List<Word> vocabulary = [
+    for (int i = 0; i < 50; i++)
+      Word(
+        word: 'abandon',
+        pronunciation: 'əˈbændən',
+        definition: ['v. 放弃，抛弃'],
+        enExample: ['He abandoned his family.', 'He abandoned his family.'],
+        chExample: ['他抛弃了他的家人。', '他抛弃了他的家人。'],
+      ),
+    // Add more words here
+  ];
+
+  Future<void> _showDeleteConfirmationDialog(int index) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Deletion'),
+          content: const Text('Are you sure you want to delete this word?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () {
+                setState(() {
+                  vocabulary.removeAt(index);
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+        title: const Text('生词本'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.95,
+          child: ListView.builder(
+            itemCount: vocabulary.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(vocabulary[index].word),
+                subtitle: Text(vocabulary[index].definition[0]),
+                onTap: () {
+                  Get.to(CollectSpec(word: vocabulary[index]));
+                },
+                onLongPress: () {
+                  _showDeleteConfirmationDialog(index);
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/collect/widget/collect_spec.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:words/word/model/word.dart';
+import 'package:words/word/wiget/audio_button.dart';
+import 'package:words/word/wiget/word_card.dart';
+import 'package:words/word/wiget/word_item.dart';
+
+class CollectSpec extends StatelessWidget {
+  final Word word;
+  const CollectSpec({
+    super.key,
+    required this.word,
+  });
+
+  List<Widget> _specContetn() {
+    return [
+      Column(
+        children: [
+          Text(
+            word.word,
+            style: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          PlayAudioButton(word: word, type: 1),
+        ],
+      ),
+      WordItem(
+        header: "释义",
+        content: word.definition
+            .map(
+              (e) => Text(
+                e,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            )
+            .toList(),
+      ),
+      WordItem(
+        header: "例句",
+        content: List.generate(
+          word.enExample.length,
+          (index) => Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                word.enExample[index],
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                word.chExample[index],
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      const SizedBox(height: 30),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(word.word),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FractionallySizedBox(
+              widthFactor: 0.9,
+              child: WordCard(contents: _specContetn()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
